b9a1709bf9baf582c3d81d91348648fc8f141490
Masashi Seki <46625106+cactusHats@users.noreply.github.com>
2020-06-25 13:39:16 +0900

    AnalogToPercentConverterの追加

---
+++ AnalogToPercentConverter/include/AnalogToPercentConverter/AnalogToPercentConverter.h
@@ -0,0 +1,286 @@
+﻿// -*- C++ -*-
+/*!
+ * @file  AnalogToPercentConverter.h
+ * @brief ModuleDescription
+ * @date  $Date$
+ *
+ * $Id$
+ */
+
+#ifndef ANALOGTOPERCENTCONVERTER_H
+#define ANALOGTOPERCENTCONVERTER_H
+
+//デバッグ用マクロ
+#define DEBUG 1
+
+#include <rtm/idl/BasicDataTypeSkel.h>
+#include <rtm/idl/ExtendedDataTypesSkel.h>
+#include <rtm/idl/InterfaceDataTypesSkel.h>
+
+// Service implementation headers
+// <rtc-template block="service_impl_h">
+
+// </rtc-template>
+
+// Service Consumer stub headers
+// <rtc-template block="consumer_stub_h">
+#include "BasicDataTypeStub.h"
+
+// </rtc-template>
+
+#include <rtm/Manager.h>
+#include <rtm/DataFlowComponentBase.h>
+#include <rtm/CorbaPort.h>
+#include <rtm/DataInPort.h>
+#include <rtm/DataOutPort.h>
+
+/*!
+ * @class AnalogToPercentConverter
+ * @brief ModuleDescription
+ *
+ */
+class AnalogToPercentConverter
+  : public RTC::DataFlowComponentBase
+{
+ public:
+  /*!
+   * @brief constructor
+   * @param manager Maneger Object
+   */
+  AnalogToPercentConverter(RTC::Manager* manager);
+
+  /*!
+   * @brief destructor
+   */
+  ~AnalogToPercentConverter();
+
+  // <rtc-template block="public_attribute">
+  
+  // </rtc-template>
+
+  // <rtc-template block="public_operation">
+  
+  // </rtc-template>
+
+  /***
+   *
+   * The initialize action (on CREATED->ALIVE transition)
+   *
+   * @return RTC::ReturnCode_t
+   * 
+   * 
+   */
+   virtual RTC::ReturnCode_t onInitialize();
+
+  /***
+   *
+   * The finalize action (on ALIVE->END transition)
+   *
+   * @return RTC::ReturnCode_t
+   * 
+   * 
+   */
+  // virtual RTC::ReturnCode_t onFinalize();
+
+  /***
+   *
+   * The startup action when ExecutionContext startup
+   *
+   * @param ec_id target ExecutionContext Id
+   *
+   * @return RTC::ReturnCode_t
+   * 
+   * 
+   */
+  // virtual RTC::ReturnCode_t onStartup(RTC::UniqueId ec_id);
+
+  /***
+   *
+   * The shutdown action when ExecutionContext stop
+   *
+   * @param ec_id target ExecutionContext Id
+   *
+   * @return RTC::ReturnCode_t
+   * 
+   * 
+   */
+  // virtual RTC::ReturnCode_t onShutdown(RTC::UniqueId ec_id);
+
+  /***
+   *
+   * The activated action (Active state entry action)
+   *
+   * @param ec_id target ExecutionContext Id
+   *
+   * @return RTC::ReturnCode_t
+   * 
+   * 
+   */
+   virtual RTC::ReturnCode_t onActivated(RTC::UniqueId ec_id);
+
+  /***
+   *
+   * The deactivated action (Active state exit action)
+   *
+   * @param ec_id target ExecutionContext Id
+   *
+   * @return RTC::ReturnCode_t
+   * 
+   * 
+   */
+   virtual RTC::ReturnCode_t onDeactivated(RTC::UniqueId ec_id);
+
+  /***
+   *
+   * The execution action that is invoked periodically
+   *
+   * @param ec_id target ExecutionContext Id
+   *
+   * @return RTC::ReturnCode_t
+   * 
+   * 
+   */
+   virtual RTC::ReturnCode_t onExecute(RTC::UniqueId ec_id);
+
+  /***
+   *
+   * The aborting action when main logic error occurred.
+   *
+   * @param ec_id target ExecutionContext Id
+   *
+   * @return RTC::ReturnCode_t
+   * 
+   * 
+   */
+  // virtual RTC::ReturnCode_t onAborting(RTC::UniqueId ec_id);
+
+  /***
+   *
+   * The error action in ERROR state
+   *
+   * @param ec_id target ExecutionContext Id
+   *
+   * @return RTC::ReturnCode_t
+   * 
+   * 
+   */
+  // virtual RTC::ReturnCode_t onError(RTC::UniqueId ec_id);
+
+  /***
+   *
+   * The reset action that is invoked resetting
+   *
+   * @param ec_id target ExecutionContext Id
+   *
+   * @return RTC::ReturnCode_t
+   * 
+   * 
+   */
+  // virtual RTC::ReturnCode_t onReset(RTC::UniqueId ec_id);
+  
+  /***
+   *
+   * The state update action that is invoked after onExecute() action
+   *
+   * @param ec_id target ExecutionContext Id
+   *
+   * @return RTC::ReturnCode_t
+   * 
+   * 
+   */
+  // virtual RTC::ReturnCode_t onStateUpdate(RTC::UniqueId ec_id);
+
+  /***
+   *
+   * The action that is invoked when execution context's rate is changed
+   *
+   * @param ec_id target ExecutionContext Id
+   *
+   * @return RTC::ReturnCode_t
+   * 
+   * 
+   */
+  // virtual RTC::ReturnCode_t onRateChanged(RTC::UniqueId ec_id);
+
+
+ protected:
+  // <rtc-template block="protected_attribute">
+  
+  // </rtc-template>
+
+  // <rtc-template block="protected_operation">
+  
+  // </rtc-template>
+
+  // Configuration variable declaration
+  // <rtc-template block="config_declare">
+  /*!
+   * Arduinoから送られてくるアナログ値の最大値を設定．
+   * DCモータの発電量は微弱なため，最大値を調整できるようにする．
+   * （本来0-1023だが，オペアンプの関係上，現在の最大値は約730）
+   * - Name: analogMaxVal analogMaxVal
+   * - DefaultValue: 700
+   * - Unit: bit
+   * - Range: 0-730
+   */
+  int m_analogMaxVal;
+
+  // </rtc-template>
+
+  // DataInPort declaration
+  // <rtc-template block="inport_declare">
+  RTC::TimedFloatSeq m_analogData;
+  /*!
+   */
+  RTC::InPort<RTC::TimedFloatSeq> m_analogDataIn;
+  
+  // </rtc-template>
+
+
+  // DataOutPort declaration
+  // <rtc-template block="outport_declare">
+  RTC::TimedLong m_percent;
+  /*!
+   */
+  RTC::OutPort<RTC::TimedLong> m_percentOut;
+  
+  // </rtc-template>
+
+  // CORBA Port declaration
+  // <rtc-template block="corbaport_declare">
+  
+  // </rtc-template>
+
+  // Service declaration
+  // <rtc-template block="service_declare">
+  
+  // </rtc-template>
+
+  // Consumer declaration
+  // <rtc-template block="consumer_declare">
+  
+  // </rtc-template>
+
+ private:
+	 int analogData[6]; //Arduinoから届いたアナログデータを一時格納する変数 
+	 int percent; //パーセンテージ
+	 int data[20];
+	 int percent_ave;
+
+  // <rtc-template block="private_attribute">
+  
+  // </rtc-template>
+
+  // <rtc-template block="private_operation">
+  
+  // </rtc-template>
+
+};
+
+
+extern "C"
+{
+  DLL_EXPORT void AnalogToPercentConverterInit(RTC::Manager* manager);
+};
+
+#endif // ANALOGTOPERCENTCONVERTER_H
---
+++ AnalogToPercentConverter/src/AnalogToPercentConverter.cpp
@@ -0,0 +1,237 @@
+﻿// -*- C++ -*-
+/*!
+ * @file  AnalogToPercentConverter.cpp
+ * @brief ModuleDescription
+ * @date $Date$
+ *
+ * $Id$
+ */
+
+#include "AnalogToPercentConverter.h"
+
+using namespace std;
+
+// Module specification
+// <rtc-template block="module_spec">
+static const char* analogtopercentconverter_spec[] =
+  {
+    "implementation_id", "AnalogToPercentConverter",
+    "type_name",         "AnalogToPercentConverter",
+    "description",       "ModuleDescription",
+    "version",           "1.0.0",
+    "vendor",            "VenderName",
+    "category",          "Category",
+    "activity_type",     "PERIODIC",
+    "kind",              "DataFlowComponent",
+    "max_instance",      "1",
+    "language",          "C++",
+    "lang_type",         "compile",
+    // Configuration variables
+    "conf.default.analogMaxVal", "700",
+
+    // Widget
+    "conf.__widget__.analogMaxVal", "text",
+    // Constraints
+
+    "conf.__type__.analogMaxVal", "int",
+
+    ""
+  };
+// </rtc-template>
+
+/*!
+ * @brief constructor
+ * @param manager Maneger Object
+ */
+AnalogToPercentConverter::AnalogToPercentConverter(RTC::Manager* manager)
+    // <rtc-template block="initializer">
+  : RTC::DataFlowComponentBase(manager),
+    m_analogDataIn("analogData", m_analogData),
+    m_percentOut("percent", m_percent)
+
+    // </rtc-template>
+{
+}
+
+/*!
+ * @brief destructor
+ */
+AnalogToPercentConverter::~AnalogToPercentConverter()
+{
+}
+
+
+
+RTC::ReturnCode_t AnalogToPercentConverter::onInitialize()
+{
+  // Registration: InPort/OutPort/Service
+  // <rtc-template block="registration">
+  // Set InPort buffers
+  addInPort("analogData", m_analogDataIn);
+  
+  // Set OutPort buffer
+  addOutPort("percent", m_percentOut);
+  
+  // Set service provider to Ports
+  
+  // Set service consumers to Ports
+  
+  // Set CORBA Service Ports
+  
+  // </rtc-template>
+
+  // <rtc-template block="bind_config">
+  // Bind variables and configuration variable
+  bindParameter("analogMaxVal", m_analogMaxVal, "700");
+  // </rtc-template>
+
+  cout << "analogToDigitalConverter is read!" << endl;
+  
+  return RTC::RTC_OK;
+}
+
+/*
+RTC::ReturnCode_t AnalogToPercentConverter::onFinalize()
+{
+  return RTC::RTC_OK;
+}
+*/
+
+/*
+RTC::ReturnCode_t AnalogToPercentConverter::onStartup(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+*/
+
+/*
+RTC::ReturnCode_t AnalogToPercentConverter::onShutdown(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+*/
+
+
+RTC::ReturnCode_t AnalogToPercentConverter::onActivated(RTC::UniqueId ec_id)
+{
+    //アナログデータ初期化
+    for (int i = 0; i < 6; i++) {
+        analogData[i] = 0;
+    }
+    //パーセンテージ初期化
+    percent = 0;
+    percent_ave = 0;
+
+    //配列初期化
+    for (int i = 0; i < 20; i++) {
+        data[i] = 0;
+    }
+
+  return RTC::RTC_OK;
+}
+
+
+RTC::ReturnCode_t AnalogToPercentConverter::onDeactivated(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+
+
+RTC::ReturnCode_t AnalogToPercentConverter::onExecute(RTC::UniqueId ec_id)
+{
+    //Arduinoからのアナログデータを受信
+    if (m_analogDataIn.isNew()) {
+        //データの読み込み
+        m_analogDataIn.read();
+        //この時点で入力データがm_analogDataInに格納される
+        for (int i = 0; i < m_analogData.data.length(); i++) {
+            //入力データを別変数に格納
+            if (i < 6) {
+                analogData[i] = m_analogData.data[i];
+            }
+        }
+        
+        //最大値を超えたら修正
+        if (analogData[0] > m_analogMaxVal) analogData[0] = m_analogMaxVal;
+
+        //(A0について)0-100%に変換
+        percent = double(100.0 / m_analogMaxVal) * analogData[0];
+
+        //平滑化処理
+        for (int i = 0; i < 19; i++) {
+            data[19 - i] = data[19 - i - 1];
+        }
+        data[0] = percent;
+
+        //配列更新
+        for (int i = 0; i < 20; i++)
+            percent_ave += data[i];
+        percent_ave /= 20;
+
+    }
+
+    //次のRTCへ送信
+    //m_percent.data = percent;
+    m_percent.data = percent_ave;
+    m_percentOut.write();
+
+    //デバッグ出力
+    if (DEBUG) {
+        //cout << analogData[0] <<" " << m_analogMaxVal << endl;
+        cout << "A0 = " << analogData[0] << "\tAve = " << percent_ave << "\t" << percent << "%" << endl;
+    }
+
+  return RTC::RTC_OK;
+}
+
+/*
+RTC::ReturnCode_t AnalogToPercentConverter::onAborting(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+*/
+
+/*
+RTC::ReturnCode_t AnalogToPercentConverter::onError(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+*/
+
+/*
+RTC::ReturnCode_t AnalogToPercentConverter::onReset(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+*/
+
+/*
+RTC::ReturnCode_t AnalogToPercentConverter::onStateUpdate(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+*/
+
+/*
+RTC::ReturnCode_t AnalogToPercentConverter::onRateChanged(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+*/
+
+
+
+extern "C"
+{
+ 
+  void AnalogToPercentConverterInit(RTC::Manager* manager)
+  {
+    coil::Properties profile(analogtopercentconverter_spec);
+    manager->registerFactory(profile,
+                             RTC::Create<AnalogToPercentConverter>,
+                             RTC::Delete<AnalogToPercentConverter>);
+  }
+  
+};
+
+
